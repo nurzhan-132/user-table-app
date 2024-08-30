9805f2e611ac42577c2ecc79e25c1985580a0721
nurzhan-132 <nurzhanmomynov01@gmail.com>
2024-08-30 13:12:35 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import UserTable from "./_components/UserTable/user-table.component";
+
+function App() {
+  return (
+    <div className="App">
+      <UserTable />
+    </div>
+  );
+}
+
+export default App;
